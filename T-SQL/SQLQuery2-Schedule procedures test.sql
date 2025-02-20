4c97105db596d2691cadd08c3a5bea2a9e0958b3
Anastasia <karpovanastiya@mail.ru>
2025-02-20 22:05:49 +0300

    Alternating day DONE

--- T-SQL/SQLQuery2-Schedule procedures test.sql
+++ T-SQL/SQLQuery2-Schedule procedures test.sql
@@ -1,7 +1,18 @@
 ﻿USE PV_319_Import;
+SET DATEFIRST 1;
 GO
 
-EXEC sp_ScheduleForStacionarGroup N'PV_319', N'%UML%', N'Ковтун', N'2024-08-10', N'18:30';
+--EXEC sp_ScheduleForStacionarGroup N'PV_319', N'%UML%', N'Ковтун', N'2024-08-10', N'18:30';
+--EXEC sp_ScheduleForStacionarGroup N'PD_321', N'%MS SQL Server%', N'Покидюк', N'2024-06-01', N'13:30';
 
-EXEC sp_PrintScheduleForGroup N'PV_319';
+--EXEC sp_PrintScheduleForGroup N'PD_321';
+--EXEC sp_PrintScheduleForGroupPeriod N'PV_319', '2024-06-01', '2024-07-01';
+--EXEC sp_PrintScheduleForGroupPeriod N'PD_321', '2024-06-01', '2024-07-01';
+
+--DECLARE @salary SMALLMONEY;
+--EXEC @salary = sp_CountTeachersMonthSalary N'Ковтун', 2024, 06;
+--PRINT (@salary)
 --SELECT * FROM Schedule;
+
+--DROP PROCEDURE sp_ScheduleForBaseStacionarGroup;
+EXEC sp_ScheduleForBaseStacionarGroup N'PV_319', N'Процедурное программирование %', N'Ковтун', N'2023-11-23', '18:30', 2,4,6,2,1; 
