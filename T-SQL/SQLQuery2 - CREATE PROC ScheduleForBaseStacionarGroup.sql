dbbd533071a5fdc8624d121ec4aa383f644d0877
Anastasia <karpovanastiya@mail.ru>
2025-02-23 17:27:37 +0300

    Alternating day DONE

--- T-SQL/SQLQuery2 - CREATE PROC ScheduleForBaseStacionarGroup.sql
+++ T-SQL/SQLQuery2 - CREATE PROC ScheduleForBaseStacionarGroup.sql
@@ -3,7 +3,7 @@ SET DATEFIRST 1;
 GO
 
 
-CREATE PROCEDURE sp_ScheduleForBaseStacionarGroup
+ALTER PROCEDURE sp_ScheduleForBaseStacionarGroup
 	@group_name					NVARCHAR(16),
 	@discipline_name			NVARCHAR(150),
 	--@discipline_2				NVARCHAR(150),
@@ -31,6 +31,7 @@ BEGIN
 	BEGIN
 		PRINT(@date);
 		PRINT(DATENAME(WEEKDAY, @date));
+		PRINT(DATEPART(WEEKDAY, @date));
 		PRINT(@lesson_number+1);
 		PRINT(@time);
 		SET @lesson_number = @lesson_number+1;
@@ -38,14 +39,14 @@ BEGIN
 		PRINT(DATEADD(MINUTE, 95, @time));
 		SET @lesson_number = @lesson_number+1;
 		PRINT('-----------------------------------');
-		PRINT(DATENAME(WEEKDAY, @date));
+		PRINT(DATEPART(WEEKDAY, @date));
 		PRINT(@alternating_day);
 
 		IF (DATEPART(WEEKDAY, @date)=@alternating_day)
 		BEGIN
 			SET @date = DATEADD(DAY, 2, @date);
 		END
-		IF (DATEPART(WEEKDAY, @date)=4)
+		ELSE IF (DATEPART(WEEKDAY, @date)=4)
 		BEGIN
 			PRINT('IIF:');
 			PRINT(DATEPART(WEEKDAY, @date));
