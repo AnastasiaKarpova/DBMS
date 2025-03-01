678355f26cf9ac8631d983d50f0616e8cd3e7f44
Anastasia <karpovanastiya@mail.ru>
2025-03-01 20:38:19 +0300

    Holidays in progress

--- T-SQL/SQLQuery7 - CREATE PROCEDURE NewYear.sql
+++ T-SQL/SQLQuery7 - CREATE PROCEDURE NewYear.sql
@@ -2,15 +2,24 @@ USE PV_319_Import;
 SET DATEFIRST 1;
 GO
 
-DECLARE @date	AS DATE = DATEFROMPARTS(2025,01,01);
-DECLARE @weekday	AS	TINYINT	= DATEPART(WEEKDAY, @date);
-DECLARE @start_date	AS	DATE = DATEADD(DAY, -@weekday+1, @date);
-PRINT(@date);
-PRINT(@weekday);
-PRINT(@start_date);
+CREATE PROCEDURE sp_AddNewYearHolidayFor
+	@year	AS	SMALLINT
+BEGIN
+	DECLARE @date		AS	DATE	= DATEFROMPARTS(2025,01,01);
+	DECLARE @weekday	AS	TINYINT	= DATEPART(WEEKDAY, @date);
+	DECLARE @start_date	AS	DATE	= DATEADD(DAY, -@weekday+1, @date);
+	DECLARE @date		AS	DATE	= @start_date;
+	DECLARE @duration	AS	TINYINT	= (SELECT duration FROM Holidays WHERE holiday_id=1);
+	--DECLARE @day		AS	TINYINT = 0;
 
---CREATE PROCEDURE sp_AddNewYearHolidayFor
---	@year	AS	SMALLINT
---BEGIN
---	DECLARE @start_date	AS	DATE = DATEADD()
---END
+	WHILE @duration > 0
+	BEGIN
+		INSERT DaysOFF ([date], holiday)	VALUES (@date, 1);
+		SET		@date = DATEADD(DAY, 1, @date);
+		SET		@duration = @duration - 1;
+	END
+	--PRINT(@date);
+	--PRINT(@weekday);
+	--PRINT(@start_date);
+
+END
