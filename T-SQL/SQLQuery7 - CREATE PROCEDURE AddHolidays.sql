026400308afadabbdc6396bd54225d7a38373c17
Anastasia <karpovanastiya@mail.ru>
2025-03-06 19:16:55 +0300

    GetEasterDate DONE

--- T-SQL/SQLQuery7 - CREATE PROCEDURE AddHolidays.sql
+++ T-SQL/SQLQuery7 - CREATE PROCEDURE AddHolidays.sql
@@ -7,9 +7,24 @@ CREATE PROCEDURE sp_AddHolidays
 @name	AS	NVARCHAR(150)
 AS
 BEGIN
-	DECLARE	@start_date	AS	DATE;
-	DECLARE @duration	AS	TINYINT;
-	CASE	@name
-			WHEN	@name	LIKE	(N'Нов%каникулы')	THEN	SET	@start_date = dbo.GetNewYearHolidaysStartDate(@year); SET @duration = dbo.GetHolidayDuration() END
-	CASE	WHEN	@name	LIKE	(N'Лет%каникулы')	THEN	SET	@start_date = dbo.GetSummerHolidaysStart(@year); SET @duration = dbo.GetHolidayDuration() END
+	DECLARE	
+		@start_date	AS	DATE	= dbo.GetHolidaysStartDate(@name, @year),
+		@duration	AS	TINYINT = dbo.GetHolidayDuration(@name),
+		@holiday_id AS	TINYINT = dbo.GetHolidayID(@name);
+
+	DECLARE 
+		@date	AS DATE		= @start_date,
+		@day	AS TINYINT	= 1;
+		WHILE @day < @duration
+		BEGIN
+			INSERT DaysOFF
+					([date],holiday)
+			VALUES	(@date, @holiday_id);
+			SET	@day += 1; 
+			SET	@date = DATEADD(DAY, 1, @date);
+		END
+	--CASE	@name
+	--		WHEN	@name	LIKE	(N'Нов%каникулы')	THEN	SET	@start_date = dbo.GetNewYearHolidaysStartDate(@year);	SET @duration = dbo.GetHolidayDuration(@name) END
+	--CASE	WHEN	@name	LIKE	(N'Лет%каникулы')	THEN	SET	@start_date = dbo.GetSummerHolidaysStart(@year);		SET @duration = dbo.GetHolidayDuration(@name) END
+
 END
