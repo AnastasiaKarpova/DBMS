6a63ce5308fe401a43cdee7381494fc49a594ddc
Anastasia <karpovanastiya@mail.ru>
2025-03-11 21:26:34 +0300

    TODO: Weekdays Hardcore

--- T-SQL/SQLQuery2-CREATE PROC ScheduleForStacionarGroup .sql
+++ T-SQL/SQLQuery2-CREATE PROC ScheduleForStacionarGroup .sql
@@ -24,28 +24,33 @@ BEGIN
 			PRINT(DATENAME(WEEKDAY, @date));
 			PRINT(@lesson);
 			PRINT(@time);
-	--First lesson per day:
-			IF NOT EXISTS (SELECT * FROM Schedule WHERE [group]=@group AND discipline=@discipline AND [date]=@date AND [time]=@time)
+
+			IF NOT EXISTS (SELECT [date] FROM DaysOFF WHERE [date]=@date)
 			BEGIN
-				INSERT Schedule
-						([group], discipline, teacher, [date], [time], spent)
-				VALUES	(@group, @discipline, @teacher, @date, @time, IIF(@date < GETDATE(), 1, 0));
+		--First lesson per day:
+				IF NOT EXISTS (SELECT * FROM Schedule WHERE [group]=@group AND [date]=@date AND [time]=@time) --AND discipline=@discipline )
+				BEGIN
+					INSERT Schedule
+							([group], discipline, teacher, [date], [time], spent)
+					VALUES	(@group, @discipline, @teacher, @date, @time, IIF(@date < GETDATE(), 1, 0));
+					SET @lesson = @lesson+1
+				END
+		--IIF(condition, value_1, value_2);
+
+				;
+				PRINT(@lesson);
+				PRINT(DATEADD(MINUTE, 95, @time));
+
+		--Second lesson per day:
+				IF NOT EXISTS (SELECT * FROM Schedule WHERE [group]=@group AND [date]=@date AND [time]=DATEADD(MINUTE, 95, @time)) --AND discipline=@discipline 
+				BEGIN
+					INSERT Schedule
+							([group], discipline, teacher, [date], [time], spent)
+					VALUES	(@group, @discipline, @teacher, @date, DATEADD(MINUTE, 95, @time), IIF(@date < GETDATE(), 1, 0));
+					SET @lesson = @lesson+1;
+				END
+				
 			END
-	--IIF(condition, value_1, value_2);
-
-			SET @lesson = @lesson+1;
-			PRINT(@lesson);
-			PRINT(DATEADD(MINUTE, 95, @time));
-
-	--Second lesson per day:
-			IF NOT EXISTS (SELECT * FROM Schedule WHERE [group]=@group AND discipline=@discipline AND [date]=@date AND [time]=DATEADD(MINUTE, 95, @time))
-			BEGIN
-				INSERT Schedule
-						([group], discipline, teacher, [date], [time], spent)
-				VALUES	(@group, @discipline, @teacher, @date, DATEADD(MINUTE, 95, @time), IIF(@date < GETDATE(), 1, 0));
-			END
-
-			SET @lesson = @lesson+1;
 			PRINT('--------------------------------');
 			IF(DATEPART(WEEKDAY, @date)=6)
 			BEGIN
